d484fc0e916a4b39eb8431bbaf7500a71a63f723
Sebastian Contreras <sebastian.contreras@algolia.com>
2023-03-24 10:29:37 -0500

    Adding first code upload.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,47 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useRef} from "react";
+import { SafeAreaView, StatusBar, StyleSheet, View, Text } from 'react-native';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch } from 'react-instantsearch-hooks';
+import { SearchBox } from "../components/SearchBox";
+import { InfiniteHits } from "../components/InfiniteHits";
+
+const searchClient = algoliasearch('SGF0RZXAXL', '0ac0c3b165eb3773097eca1ac25d8fdd');
 
 export default function App() {
+  const listRef = useRef(null);
+
+  function scrollToTop() {
+    listRef.current?.scrollToOffset({ animated: false, offset: 0 });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <InstantSearch searchClient={searchClient} indexName="instant_search">
+          <SearchBox onChange={scrollToTop} />
+          <InfiniteHits hitComponent={Hit} />
+        </InstantSearch>
+      </View>
+    </SafeAreaView>
   );
 }
 
+function Hit({ hit }) {
+  return (
+    <Text>{hit.name}</Text>
+  );
+}
+
+
 const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    backgroundColor: '#252b33',
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 16,
+    backgroundColor: '#ffffff',
+    flexDirection: 'column',
   },
 });
